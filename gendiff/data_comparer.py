b0c93861768ec818037a12c8e39e792c7606e0ab
David-Roklem <roklem@mail.ru>
2023-02-23 11:09:11 +0300

    add the module with general logic data_comparer.py

---
+++ gendiff/data_comparer.py
@@ -0,0 +1,48 @@
+"""
+In this module two dictionaries are compared.
+As a result the main func compare_data returns
+dictionary that represents the difference between two
+input dicts.
+"""
+
+NESTED = 'nested'
+ADDED = 'added'
+REMOVED = 'removed'
+CHANGED = 'changed'
+UNCHANGED = 'unchanged'
+
+
+def compare_data(data1:dict, data2:dict) -> dict:
+    diff = {}
+    keys1, keys2 = set(data1.keys()), set(data2.keys())
+    combained_keys = sorted(keys1 | keys2)
+    for key in combained_keys:
+        if key not in data1 and key in data2:
+            diff[key] = {
+                'type': ADDED,
+                'value': data2[key]
+            }
+        elif key in data1 and key not in data2:
+            diff[key] = {
+                'type': REMOVED,
+                'value': data1[key]
+            }
+        elif data1[key] == data2[key]:
+            diff[key] = {
+                'type': UNCHANGED,
+                'value': data2[key]
+            }
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            diff[key] = {
+                'type': NESTED,
+                'value': compare_data(data1[key], data2[key])
+            }
+        else:
+            diff[key] = {
+                'type': CHANGED,
+                'value': {
+                    'old value': data1[key],
+                    'new value': data2[key]
+                }
+            }
+    return diff
