0c23355873ef9bb27109da11584c187f59fac4ac
David-Roklem <roklem@mail.ru>
2023-03-01 11:34:19 +0300

    first fix after review - substituted duplications in the path

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from gendiff.file_reader import read_data
 from gendiff.gendiff import generate_diff
@@ -9,70 +10,33 @@ def get_answer(answer_path):
 
 
 @pytest.mark.parametrize('filepath1, filepath2, format_name, answer', [
+    ('file1.json', 'file2.json', STYLISH, 'answer_stylish_flat'),
+    ('file1.yaml', 'file2.yaml', STYLISH, 'answer_stylish_flat'),
+    ('file1.yml', 'file2.yml', STYLISH, 'answer_stylish_flat'),
     (
-        'tests/fixtures/file1.json', 'tests/fixtures/file2.json',
-        STYLISH, 'tests/fixtures/answer_stylish_flat'
+        'file1_nested.json', 'file2_nested.json',
+        STYLISH, 'answer_stylish_nested'
     ),
     (
-        'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
-        STYLISH, 'tests/fixtures/answer_stylish_flat'
+        'file1_nested.yaml', 'file2_nested.yaml',
+        STYLISH, 'answer_stylish_nested'
     ),
-    (
-        'tests/fixtures/file1.yml', 'tests/fixtures/file2.yml',
-        STYLISH, 'tests/fixtures/answer_stylish_flat'
-    ),
-    (
-        'tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json',
-        STYLISH, 'tests/fixtures/answer_stylish_nested'
-    ),
-    (
-        'tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
-        STYLISH, 'tests/fixtures/answer_stylish_nested'
-    ),
-    (
-        'tests/fixtures/file1.json', 'tests/fixtures/file2.json',
-        PLAIN, 'tests/fixtures/answer_plain_flat'
-    ),
-    (
-        'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
-        PLAIN, 'tests/fixtures/answer_plain_flat'
-    ),
-    (
-        'tests/fixtures/file1.yml', 'tests/fixtures/file2.yml',
-        PLAIN, 'tests/fixtures/answer_plain_flat'
-    ),
-    (
-        'tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json',
-        PLAIN, 'tests/fixtures/answer_plain_nested'
-    ),
-    (
-        'tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
-        PLAIN, 'tests/fixtures/answer_plain_nested'
-    ),
-    (
-        'tests/fixtures/file1.json', 'tests/fixtures/file2.json',
-        JSON, 'tests/fixtures/answer_json_flat'
-    ),
-    (
-        'tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
-        JSON, 'tests/fixtures/answer_json_flat'
-    ),
-    (
-        'tests/fixtures/file1.yml', 'tests/fixtures/file2.yml',
-        JSON, 'tests/fixtures/answer_json_flat'
-    ),
-    (
-        'tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json',
-        JSON, 'tests/fixtures/answer_json_nested'
-    ),
-    (
-        'tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
-        JSON, 'tests/fixtures/answer_json_nested'
-    ),
+    ('file1.json', 'file2.json', PLAIN, 'answer_plain_flat'),
+    ('file1.yaml', 'file2.yaml', PLAIN, 'answer_plain_flat'),
+    ('file1.yml', 'file2.yml', PLAIN, 'answer_plain_flat'),
+    ('file1_nested.json', 'file2_nested.json', PLAIN, 'answer_plain_nested'),
+    ('file1_nested.yaml', 'file2_nested.yaml', PLAIN, 'answer_plain_nested'),
+    ('file1.json', 'file2.json', JSON, 'answer_json_flat'),
+    ('file1.yaml', 'file2.yaml', JSON, 'answer_json_flat'),
+    ('file1.yml', 'file2.yml', JSON, 'answer_json_flat'),
+    ('file1_nested.json', 'file2_nested.json', JSON, 'answer_json_nested'),
+    ('file1_nested.yaml', 'file2_nested.yaml', JSON, 'answer_json_nested'),
 ])
 def test_generate_diff(filepath1, filepath2, format_name, answer):
-    answer = get_answer(answer)
+    answer = get_answer(os.path.join('tests', 'fixtures', answer))
     l_answer = len(answer)  # length answer
     assert generate_diff(
-        filepath1, filepath2, format_name
+        os.path.join('tests', 'fixtures', filepath1),
+        os.path.join('tests', 'fixtures', filepath2),
+        format_name
     ) == answer[:l_answer]
