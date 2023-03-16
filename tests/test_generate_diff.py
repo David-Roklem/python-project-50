cc529bf41dd1fcd73cfdf937173884e965e6b9ad
David-Roklem <roklem@mail.ru>
2023-03-16 17:35:44 +0300

    fifth review. Refactored test file, removed variable "head"

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,6 +9,10 @@ def get_answer(answer_path):
     return read_data(answer_path)
 
 
+def get_fixture_path(file_name):
+    return os.path.join('tests', 'fixtures', file_name)
+
+
 @pytest.mark.parametrize('filepath1, filepath2, format_name, answer', [
     ('file1.json', 'file2.json', STYLISH, 'answer_stylish_flat'),
     ('file1.yaml', 'file2.yaml', STYLISH, 'answer_stylish_flat'),
@@ -33,10 +37,9 @@ def get_answer(answer_path):
     ('file1_nested.yaml', 'file2_nested.yaml', JSON, 'answer_json_nested'),
 ])
 def test_generate_diff(filepath1, filepath2, format_name, answer):
-    head = os.path.join('tests', 'fixtures')
-    answer = get_answer(os.path.join(head, answer))
+    answer = get_answer(get_fixture_path(answer))
     assert generate_diff(
-        os.path.join(head, filepath1),
-        os.path.join(head, filepath2),
+        get_fixture_path(filepath1),
+        get_fixture_path(filepath2),
         format_name
     ) == answer
